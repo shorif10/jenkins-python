3aa570c9672799eac867308206207cd56ed80298
Shorif Mahmud Abdullah <shorif.mahmud@bjitgroup.com>
2024-08-30 12:04:44 +0600

    2nd commit

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ def subtraction(a,b):
     print(a-b)
 
 
+def multiplication(a,b):
+    print(a*b)
+
+def division(a,b):
+    print(a//b)
+
 
 if __name__ == '__main__':
     add(1,2)
     subtraction(2,3)
+    multiplication(5,2)
+    division(3,2)
